162d65b8fe475aa3f92938b122ea599a041e3a47
CaptainZippy <muchomojo@gmail.com>
2016-07-05 14:19:06 +0100

    Copy from slip6

---
+++ slip7/Pch.h
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _ITERATOR_DEBUG_LEVEL 0
+#pragma warning(disable:4996)
+#include <vector>
+#include <cctype>
+#include <string>
+#include <map>
+#include <cstdarg>
+#include <cstdint>
+#include <windows.h>
+#include <unordered_map>
+#include <unordered_set>
---
+++ slip7/test.py
@@ -0,0 +1,27 @@
+﻿
+import glob
+import os
+import time
+
+tests = (
+    ("ack", 4),
+    ("ary", ),
+    ("fibo", 5),
+    ("harmonic",100),
+    #("harmonic",10000000),
+    ("hello",),
+    ("moments",),
+    ("nestedloop",5),
+    ("nsieve",4)
+    )
+
+
+for test in tests:
+    cmd = r"x64\\Release\\slip.exe bench\\%s.slip %s" % (test[0],"".join(str(s) for s in test[1:]))
+    print("Running", cmd)
+    start = time.time()
+    if os.system(cmd) == 0:
+        end = time.time()
+        print("Time:", end-start)
+    else:
+        print("FAILED")
